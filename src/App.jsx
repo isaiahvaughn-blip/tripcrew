f50509c7af1a5e5c0067667e0a688f1e60c2cbe1
Isaiah <isaiahvaughn@gmail.com>
2026-04-20 20:41:49 -0700

    connect supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { supabase } from './supabase'
 import { useState } from "react";
 
 // ─── DATA ────────────────────────────────────────────────────────────────────
---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://pkrymzcjfsioazapllwl.supabase.co',
+  'sb_publishable_UFYUNA311sRwf-3uli9h9w_T00ijC4c'
+)
